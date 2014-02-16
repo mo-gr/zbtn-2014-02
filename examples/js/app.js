d2a4c493f5f610dde752db2cd4de6b647a9908b8
Moritz Grauel <mo@notadomain.com>
2014-02-16 22:55:05 +0100

    foaas

--- examples/js/app.js
+++ examples/js/app.js
@@ -3,11 +3,14 @@
   'use strict';
   var helloWorldApp = angular.module('HelloWorldApp', []);
 
-  helloWorldApp.controller('GreetingController', function($scope, GreetingService) {
+  helloWorldApp.controller('GreetingController', function($scope, GreetingService, TauntService) {
     $scope.greeting = "Welcome";
 
     $scope.greet = function(name) {
       $scope.greeting = GreetingService.greet(name);
+      TauntService.taunt(name).then(function(response) {
+        $scope.taunt = response.data.message;
+      });
     };
   });
 
@@ -18,6 +21,18 @@
                      : "Hello, " + name;
       }
     }
+  });
+
+  helloWorldApp.service('TauntService', function($http) {
+    return {
+      taunt: function (name) {
+        if (name) {
+          return $http.get('http://foaas.com/linus/' + name + '/from', {
+            responseType: 'text/plain'
+          });
+        }
+      }
+    };
   });
 }());
 
